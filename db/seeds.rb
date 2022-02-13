0dc251e51d4ed22212c1f863c45c91c27b0e6edd
Hamdaan Khalid <khalid.hamdaan@gmail.com>
2022-02-13 16:55:08 -0500

    fix seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -25,50 +25,50 @@ Article.create!(
     Another really cool lesson from this was that I understood the point and value of design patterns, and how they add to code extensibility. Had I never been given the freedom that un-opinionated frameworks like Express.js, Flask, or Sinatra give, I would have never seen the value of opinions and how they help build codebases that will make sense for a long time.
 
     So my advice for anyone starting out would be to choose the un-opinionated framework in the language of your choice, set to build out something big and long term so that it is guaranteed that you will make design mistakes and see code flexibility decline over time. Then when you feel that you are doing repeated tasks to configure things, learn the more opinionated framework. It is bound to make more sense.",
-  status: "public",
+  status: "public"
 )
 
 Experience.create!(
-  organization: 'Purdue University'
-  title: 'B.S. Mathematics'
-  description: 'As a mathematics undergraduate Student at Purdue. I had the opportunity to take courses including: Calculus, Linear Algebra, Real Analysis, Abstract Algebra, Numerical Analysis, Data Mine, CS-177, Statistics, and Regression Modeling'
-  started_at: '2017'
-  ended_at: '2020'
-  type_of: 'Education'
+  organization: 'Purdue University',
+  title: 'B.S. Mathematics',
+  description: 'As a mathematics undergraduate Student at Purdue. I had the opportunity to take courses including: Calculus, Linear Algebra, Real Analysis, Abstract Algebra, Numerical Analysis, Data Mine, CS-177, Statistics, and Regression Modeling',
+  started_at: '2017',
+  ended_at: '2020',
+  type_of: 'Education',
   order: 6
 )
 
 Experience.create!(
-  organization: 'Root Insurance'
-  title: 'Software Engineer'
-  description: 'I started as an intern on the data science engineering team where I was responsible for prototyping the MVP for ETL processing using Grothendeick Construction with ECS cluster, lambdas, Aws Stepfunctions, and Terraform. I switched to full time Engineer on our Data Science productivity team in February 2021 where I came up with and created Rrocket, an in-house software that enabled Data Scientists and Actuaries to write functional R programs locally, abstract away dockerizing, and transform every program to a functional lambda component that could be plugged and chugged into AWS Sagemaker processing jobs, without any involvement of an Engineer. On the Customer Engagement Team I delivered the full stack features for the projects Full Term Savings, and Secure Password that involved frontend changes to our mobile, and web application, rails backend, and included AB testing for customer engagement. I also worked on squashing bugs every sprint.'
-  started_at: 'October 2020'
-  ended_at: 'November 2021'
-  type_of: 'Job'
+  organization: 'Root Insurance',
+  title: 'Software Engineer',
+  description: 'I started as an intern on the data science engineering team where I was responsible for prototyping the MVP for ETL processing using Grothendeick Construction with ECS cluster, lambdas, Aws Stepfunctions, and Terraform. I switched to full time Engineer on our Data Science productivity team in February 2021 where I came up with and created Rrocket, an in-house software that enabled Data Scientists and Actuaries to write functional R programs locally, abstract away dockerizing, and transform every program to a functional lambda component that could be plugged and chugged into AWS Sagemaker processing jobs, without any involvement of an Engineer. On the Customer Engagement Team I delivered the full stack features for the projects Full Term Savings, and Secure Password that involved frontend changes to our mobile, and web application, rails backend, and included AB testing for customer engagement. I also worked on squashing bugs every sprint.',
+  started_at: 'October 2020',
+  ended_at: 'November 2021',
+  type_of: 'Job',
   order: 2
 )
 
 Experience.create!(
-  organization: 'Attune Inc'
-  title: 'Software Engineer'
-  description: 'Full stack product software engineer at Attune, working on our node js and Typescript backend and angular frontend. I work on onboarding flows for products on our marketplace platform.'
-  started_at: 'November 2021'
-  ended_at: 'Current'
-  type_of: 'Job'
+  organization: 'Attune Inc',
+  title: 'Software Engineer',
+  description: 'Full stack product software engineer at Attune, working on our node js and Typescript backend and angular frontend. I work on onboarding flows for products on our marketplace platform.',
+  started_at: 'November 2021',
+  ended_at: 'Current',
+  type_of: 'Job',
   order: 1
 )
 
 Experience.create!(
-  organization: 'Skills'
-  title: 'Backend & Frontend Software Engineering Skills'
+  organization: 'Skills',
+  title: 'Backend & Frontend Software Engineering Skills',
   description: 'Languages: Ruby, Python, Javascript, Typescript, Golang
   Backend Frameworks: Rails, Django, Flask, Express, Nest.
   Frontend: Javascript, Typescript, CSS, HTML.
   Frontend Frameworks: React & Angular.
   Databases: Postgresql, Sqlite, MongoDb, DynamoDb, Redis.
-  Devops: Docker, Terraform, Kubernetes.'
-  started_at:
-  ended_at:
-  type_of: 'Skills'
+  Devops: Docker, Terraform, Kubernetes.',
+  started_at: nil,
+  ended_at: nil,
+  type_of: 'Skills',
   order: 5
 )
