649d879f6075e6131ef2d3a2d95a2d4831d89de7
Hamdaan Khalid <khalid.hamdaan@gmail.com>
2022-02-13 18:11:22 -0500

    Fix urls

--- app/mailers/notify_article_published_mailer.rb
+++ app/mailers/notify_article_published_mailer.rb
@@ -5,7 +5,7 @@ class NotifyArticlePublishedMailer < ApplicationMailer
     @email = params[:email]
     @article = params[:article]
     Rails.logger.debug root_url
-    @url = "#https://hamdaan-rails-personal.herokuapp.com/articles/#{@article.id}"
+    @url = "https://hamdaan-rails-personal.herokuapp.com/articles/#{@article.id}"
     mail(to: @email, subject: "Read my latest Engineering article #{@article.title}")
   end
 end
