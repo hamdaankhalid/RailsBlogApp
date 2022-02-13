b3cb7edc61114720a70d8471ff98598bf65663bc
Hamdaan Khalid <khalid.hamdaan@gmail.com>
2022-02-12 22:57:11 -0500

    CD

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,10 +1,11 @@
+# frozen_string_literal: true
+
 Sidekiq.configure_client do |config|
-    config.redis = { url: ENV.fetch('REDIS_URL'), size: 1, network_timeout: 5 }
-  end
-  
-  Sidekiq.configure_server do |config|
-    config.redis = { url: ENV.fetch('REDIS_URL'), size: 12, network_timeout: 5 }
-  end
-  
-  Sidekiq::Extensions.enable_delay!
-  
+  config.redis = { url: ENV.fetch('REDIS_URL'), size: 1, network_timeout: 5 }
+end
+
+Sidekiq.configure_server do |config|
+  config.redis = { url: ENV.fetch('REDIS_URL'), size: 12, network_timeout: 5 }
+end
+
+Sidekiq::Extensions.enable_delay!
