d0905778c4495cbf23daaaf8937a364e6d509c9e
Hamdaan Khalid <khalid.hamdaan@gmail.com>
2022-02-13 17:57:36 -0500

    Fix redis issue

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,11 +1,16 @@
 # frozen_string_literal: true
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: ENV.fetch('REDIS_URL'), size: 1, network_timeout: 5 }
+  config.redis = { url: ENV.fetch('REDIS_URL'), size: 1, network_timeout: 5,
+                   ssl_params: {
+                     verify_mode: OpenSSL::SSL::VERIFY_NONE
+                   } }
 end
 
 Sidekiq.configure_server do |config|
-  config.redis = { url: ENV.fetch('REDIS_URL'), size: 12, network_timeout: 5 }
+  config.redis = { url: ENV.fetch('REDIS_URL'), size: 12, network_timeout: 5, ssl_params: {
+    verify_mode: OpenSSL::SSL::VERIFY_NONE
+  } }
 end
 
 Sidekiq::Extensions.enable_delay!
