4a526174c7bd67bc0701613c27eb6be5b1cdd0d6
Hamdaan Khalid <khalid.hamdaan@gmail.com>
2022-03-04 23:18:05 -0800

    fix linting on main branch

--- app/controllers/public/projects_controller.rb
+++ app/controllers/public/projects_controller.rb
@@ -1,5 +1,9 @@
-class Public::ProjectsController < ApplicationController
-  def index
-    @personal_projects = PersonalProject.all.order(created_at: :desc)
+# frozen_string_literal: true
+
+module Public
+  class ProjectsController < ApplicationController
+    def index
+      @personal_projects = PersonalProject.all.order(created_at: :desc)
+    end
   end
 end
--- app/helpers/public/projects_helper.rb
+++ app/helpers/public/projects_helper.rb
@@ -1,2 +1,6 @@
-module Public::ProjectsHelper
+# frozen_string_literal: true
+
+module Public
+  module ProjectsHelper
+  end
 end
