f2ea167996b71416bbded4ec5ac30263f4e12fb9
Hamdaan Khalid <42720645+hamdaankhalid@users.noreply.github.com>
2022-03-08 19:29:27 -0800

    Update schedule_post_job.rb

--- app/jobs/schedule_post_job.rb
+++ app/jobs/schedule_post_job.rb
@@ -5,8 +5,6 @@ class SchedulePostJob < ApplicationJob
 
   def perform(id)
     linkedin_schedule = Internal::LinkedinScheduler.find(id)
-    #return unless linkedin_schedule.created_at == linkedin_schedule.updated_at
-
     url = build_url(linkedin_schedule.article.id)
     li_client = Internal::LinkedinClientService.new(ENV['LI_TOKEN'])
     li_client.share_article(linkedin_schedule.article.title, linkedin_schedule.post_body, url)
