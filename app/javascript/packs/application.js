4b401bb3c2f08018ae08e7739fd2bf08c8e18179
Hamdaan Khalid <khalid.hamdaan@gmail.com>
2022-03-09 21:51:53 -0800

    webpack yaml fix

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -20,19 +20,18 @@ import "bootstrap"
 // Import the specific modules you may need (Modal, Alert, etc)
 import { Navbar, Collapse } from "bootstrap"
 
-// The stylesheet location we created earlier
-require("../stylesheets/application.scss")
+require("../../assets/stylesheets/application.scss")
 
 // If you're using Turbolinks. Otherwise simply use: jQuery(function () {
-// document.addEventListener("turbolinks:load", () => {
-//     // Both of these are from the Bootstrap 5 docs
-//     var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
-//     var tooltipList = tooltipTriggerList.map(function(tooltipTriggerEl) {
-//         return new Tooltip(tooltipTriggerEl)
-//     })
+document.addEventListener("turbolinks:load", () => {
+    // Both of these are from the Bootstrap 5 docs
+    var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+    var tooltipList = tooltipTriggerList.map(function(tooltipTriggerEl) {
+        return new Tooltip(tooltipTriggerEl)
+    })
 
-//     var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
-//     var popoverList = popoverTriggerList.map(function(popoverTriggerEl) {
-//         return new Popover(popoverTriggerEl)
-//     })
-// })
+    var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+    var popoverList = popoverTriggerList.map(function(popoverTriggerEl) {
+        return new Popover(popoverTriggerEl)
+    })
+})
