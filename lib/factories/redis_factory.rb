db7f56595e0314a01f56e2fd1fb982ca8444afa2
Hamdaan Khalid <khalid.hamdaan@gmail.com>
2022-03-12 23:48:20 -0800

    add and query methods in data store complete

---
+++ lib/factories/redis_factory.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+module Factories
+  class RedisFactory
+    def self.create
+      Thread.current[:redis] ||= Redis.new(url: ENV.fetch('REDIS_URL', 'redis://localhost:6379'))
+    end
+  end  
+end
--- spec/services/real_time_geo_system/geo_spatial_data_store_service_spec.rb
+++ spec/services/real_time_geo_system/geo_spatial_data_store_service_spec.rb
@@ -3,36 +3,64 @@
 require 'rails_helper'
 
 RSpec.describe RealTimeGeoSystem::GeoSpatialDataStoreService do
-  let(:test_client){ RedisFactory.create }
+  let(:test_client) { Factories::RedisFactory.create }
 
   before(:each) do
     test_client.flushall
   end
-  describe 'insert' do
-    it 'inserts data in geo db when valid params are passed' do
+
+  describe 'add' do
+    it 'adds data in geo db when valid params are passed' do
       data_store = RealTimeGeoSystem::GeoSpatialDataStoreService.new(test_client)
-      response = data_store.insert({
-        :querier_id => "1abcde",
-        :queriable_id => "a",
-        :longitude => 69,
-        :latitude => 69
-      })
+      response = data_store.add({
+                                  querier_id: '1abcde',
+                                  queriable_id: 'a',
+                                  longitude: 69,
+                                  latitude: 69
+                                })
       expect(response).to eq(1)
     end
 
     it 'throws argumet exception when in-valid params are passed' do
       data_store = RealTimeGeoSystem::GeoSpatialDataStoreService.new(test_client)
-      expect { data_store.insert({
-        :longitude => 69,
-        :latitude => 69
-      }) }.to raise_error(an_instance_of(ArgumentError))
+      expect do
+        data_store.add({
+                         longitude: 69,
+                         latitude: 69
+                       })
+      end.to raise_error(an_instance_of(ArgumentError))
     end
   end
 
   describe 'query' do
     it 'queries' do
-      #data_store = RealTimeGeoSystem::GeoSpatialDataStoreService.new(MyRedisWrapper.Redis)
-      #data_store.query
+      data_store = RealTimeGeoSystem::GeoSpatialDataStoreService.new(test_client)
+      added = data_store.add({
+                               querier_id: '1abcde',
+                               queriable_id: 'a',
+                               longitude: 69,
+                               latitude: 69
+                             })
+      expect(added).to eq(1)
+      query_result = data_store.query({
+                                        longitude: 69,
+                                        latitude: 69,
+                                        radius: 4,
+                                        limit: 1,
+                                        unit: 'mi',
+                                        querier_id: '1abcde'
+                                      })
+      expect(query_result).to eq([["a", "0.0001"]])
+    end
+
+    it 'throws argumet exception when in-valid params are passed' do
+      data_store = RealTimeGeoSystem::GeoSpatialDataStoreService.new(test_client)
+      expect do
+        data_store.query({
+                           longitude: 69,
+                           latitude: 69
+                         })
+      end.to raise_error(an_instance_of(ArgumentError))
     end
   end
 end
