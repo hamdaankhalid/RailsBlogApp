2a9d73642131b19761e0d00bf3a069624b8dd618
Hamdaan Khalid <khalid.hamdaan@gmail.com>
2022-03-12 22:53:29 -0800

    geo data store abstraction insertion complete

--- app/services/real_time_geo_system/geo_spatial_data_store_service.rb
+++ app/services/real_time_geo_system/geo_spatial_data_store_service.rb
@@ -2,16 +2,24 @@
 
 module RealTimeGeoSystem
   class GeoSpatialDataStoreService
-    def intialize(db)
+    def initialize(db)
       @db = db
     end
 
-    def insert(param_object)
-      @db.geoadd(param_object)
+    def insert(params)
+      validate_insert(params)
+      @db.geoadd(params[:querier_id], [params[:latitude], params[:longitude], params[:queriable_id]])
     end
 
     def query(query_object)
-      @db.georadius(query)
+      puts query_object
+      #@db.georadius(query)
+    end
+
+    private
+
+    def validate_insert(params)
+      raise ArgumentError, 'Argument hash must contain querier_id, latitude, longitude, and queriable_id keys' unless params.has_key?(:querier_id) &&  params.has_key?(:latitude) &&  params.has_key?(:longitude) &&  params.has_key?(:queriable_id)
     end
   end
 end
--- spec/services/real_time_geo_system/geo_spatial_data_store_service_spec.rb
+++ spec/services/real_time_geo_system/geo_spatial_data_store_service_spec.rb
@@ -3,17 +3,36 @@
 require 'rails_helper'
 
 RSpec.describe RealTimeGeoSystem::GeoSpatialDataStoreService do
+  let(:test_client){ RedisFactory.create }
+
+  before(:each) do
+    test_client.flushall
+  end
   describe 'insert' do
-    it 'inserts valid data' do
-      data_store = RealTimeGeoSystem::GeoSpatialDataStoreService.new
-      data_store.insert
+    it 'inserts data in geo db when valid params are passed' do
+      data_store = RealTimeGeoSystem::GeoSpatialDataStoreService.new(test_client)
+      response = data_store.insert({
+        :querier_id => "1abcde",
+        :queriable_id => "a",
+        :longitude => 69,
+        :latitude => 69
+      })
+      expect(response).to eq(1)
+    end
+
+    it 'throws argumet exception when in-valid params are passed' do
+      data_store = RealTimeGeoSystem::GeoSpatialDataStoreService.new(test_client)
+      expect { data_store.insert({
+        :longitude => 69,
+        :latitude => 69
+      }) }.to raise_error(an_instance_of(ArgumentError))
     end
   end
 
   describe 'query' do
     it 'queries' do
-      data_store = RealTimeGeoSystem::GeoSpatialDataStoreService.new
-      data_store.query
+      #data_store = RealTimeGeoSystem::GeoSpatialDataStoreService.new(MyRedisWrapper.Redis)
+      #data_store.query
     end
   end
 end
