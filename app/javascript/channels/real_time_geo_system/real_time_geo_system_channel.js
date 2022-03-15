f3d18e27e1faaae5de65fce2a2659d93d8428da6
Hamdaan Khalid <khalid.hamdaan@gmail.com>
2022-03-14 22:52:14 -0700

    initial logic layout for client websocket

--- app/javascript/channels/real_time_geo_system/real_time_geo_system_channel.js
+++ app/javascript/channels/real_time_geo_system/real_time_geo_system_channel.js
@@ -1,25 +1,67 @@
-import consumer from "../consumer"
+import consumer from "../consumer";
 
-const GEO_SYSTEM_PATH = '/real_time_geo_system';
+const GEO_SYSTEM_PATHS = [
+  new URLPattern({ pathname: "/real_time_geo_system/:id" }),
+  new URLSearchParams({ pathname: "/real_time_geo_system/querier_panel/:id" }),
+];
 
 document.addEventListener("turbolinks:load", function () {
-  if (window.location.pathname !== GEO_SYSTEM_PATH) {
+  if (!isPathMatch(window.location.pathname)) {
     return;
   }
 
-  consumer.subscriptions.create("RealTimeGeoSystem::RealTimeGeoSystemChannel", {
-    connected() {
-      this.perform('query',{querier_id: "DATA BEING SENT" });
-      // Called when the subscription is ready for use on the server
-    },
-  
-    disconnected() {
-      // Called when the subscription has been terminated by the server
-    },
-  
-    received(data) {
-      // Called when there's incoming data on the websocket for this channel
-      console.log(data);
+  const realTimeGeoSystem = consumer.subscriptions.create(
+    "RealTimeGeoSystem::RealTimeGeoSystemChannel",
+    {
+      connected() {
+        // Called when the subscription is ready for use on the server
+      },
+
+      disconnected() {
+        // Called when the subscription has been terminated by the server
+      },
+
+      received(data) {
+        // Called when there's incoming data on the websocket for this channel
+        // if data is recieved is for querier panel go ahead and updateQueryPanel
+        // ignore data for querier panel unless querier has turned on the flag
+      },
+
+      query() {
+        this.perform("query", { querier_id: "DATA BEING SENT" });
+      },
+
+      sendLocation() {
+        // used by a queriable on location update to hit the server with their current location
+      },
     }
-  });  
+  );
+
+  // Querier_Panel
+  function startQuerying() {
+    // takes data from form and invokes a method
+  }
+
+  function updateQueryPanel() {
+    // display changes on UI
+  }
+
+  // Queriables
+  function startTracking() {
+    // send data to realTimeGeoSystem subscription every time location updates
+  }
+
+  function stopTracking() {
+    // terminate realTimeGeoSystem subscription
+  }
+
+  // Helper used for defining which pages to intialize the websocket connections on, with wildcard matching for id
+  function isPathMatch(path) {
+    for (let pattern of GEO_SYSTEM_PATHS) {
+      if (pattern.test(path)) {
+        return true;
+      }
+    }
+    return false;
+  }
 });
