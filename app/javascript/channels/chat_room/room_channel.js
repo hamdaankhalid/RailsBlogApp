6a8340c714eb9e98117684ce7357fce2fa1e0b76
Hamdaan Khalid <khalid.hamdaan@gmail.com>
2022-03-14 17:39:17 -0700

    Fix channel js config

--- app/javascript/channels/chat_room/room_channel.js
+++ app/javascript/channels/chat_room/room_channel.js
@@ -1,11 +1,16 @@
-import consumer from "../consumer"
+import consumer from "../consumer";
 
-document.addEventListener('readystatechange', function() {
-  if (document.readyState === "complete") {
-  const messages = document.getElementById('messages');
+const CHAT_ROOM_PATH = '/chat-room';
+
+document.addEventListener("turbolinks:load", function () {
+  if (window.location.pathname !== CHAT_ROOM_PATH) {
+    return;
+  }
+
+  const messages = document.getElementById("messages");
   messages.scrollTop = messages.scrollHeight;
-  const author = document.getElementById('author');
-  const chatSubmitButton = document.getElementById('chat-submit');
+  const author = document.getElementById("author");
+  const chatSubmitButton = document.getElementById("chat-submit");
 
   const chatter = consumer.subscriptions.create("ChatRoom::RoomChannel", {
     connected() {
@@ -22,40 +27,35 @@ document.addEventListener('readystatechange', function() {
       appendMessage(data);
     },
 
-    speak: function(data) {
-      return this.perform('speak', data);
-    }
-});
-
-function appendMessage(data){
-  const lilDiv = document.createElement('div');
-  const styledAuthor = document.createElement('span');
-  const author = document.createTextNode(`${data.author}:`);
-  styledAuthor.style.fontWeight = "bold";
-  styledAuthor.appendChild(author);
-
-  lilDiv.appendChild(styledAuthor);
-  lilDiv.appendChild(
-    document.createTextNode(`\u00A0${data.content}`)
-  );
-  lilDiv.style = "display: inline-flex";
-  lilDiv.class = "message"
-
-  messages.appendChild(
-    lilDiv
-  );
-
-  messages.scrollTop = messages.scrollHeight;
-}
-
-// event listeners
-chatSubmitButton.addEventListener('click', function(event) {
-  event.preventDefault();
-  chatter.speak({
-    "content": message.value,
-    "author": author.value
+    speak: function (data) {
+      return this.perform("speak", data);
+    },
   });
-  message.value = '';
-});
 
-}});
+  function appendMessage(data) {
+    const lilDiv = document.createElement("div");
+    const styledAuthor = document.createElement("span");
+    const author = document.createTextNode(`${data.author}:`);
+    styledAuthor.style.fontWeight = "bold";
+    styledAuthor.appendChild(author);
+
+    lilDiv.appendChild(styledAuthor);
+    lilDiv.appendChild(document.createTextNode(`\u00A0${data.content}`));
+    lilDiv.style = "display: inline-flex";
+    lilDiv.class = "message";
+
+    messages.appendChild(lilDiv);
+
+    messages.scrollTop = messages.scrollHeight;
+  }
+
+  // event listeners
+  chatSubmitButton.addEventListener("click", function (event) {
+    event.preventDefault();
+    chatter.speak({
+      content: message.value,
+      author: author.value,
+    });
+    message.value = "";
+  });
+});
--- app/javascript/channels/index.js
+++ app/javascript/channels/index.js
@@ -4,13 +4,3 @@
 const channels = require.context('.', true, /_channel\.js$/)
 channels.keys().forEach(channels)
 
-/*
-Previous config
-// Load all the channels within this directory and all subdirectories.
-// Channel files must be named *_channel.js.
-
-// const channels = require.context('./chat_room', true, /_channel\.js$/)
-// channels.keys().forEach(channels)
-require('./chat_room/room_channel.js');
-// require('./consumer.js')
-*/
