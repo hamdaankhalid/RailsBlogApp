2646d940b510c9f48f9e8fe706c28e7ac8887719
Hamdaan Khalid <khalid.hamdaan@gmail.com>
2022-03-14 18:54:59 -0700

    add basic tests

--- spec/requests/real_time_geo_system/real_time_geo_system_spec.rb
+++ spec/requests/real_time_geo_system/real_time_geo_system_spec.rb
@@ -3,9 +3,23 @@
 require 'rails_helper'
 
 RSpec.describe 'RealTimeGeoSystem::RealTimeGeoSystems', type: :request do
-  describe 'GET /' do
+  describe 'GET /real_time_geo_system' do
     it 'returns http success' do
       get '/real_time_geo_system'
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe 'GET /real_time_geo_system/:id' do
+    it 'returns http success' do
+      get '/real_time_geo_system/1234'
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe 'GET /real_time_geo_system/querier_panel/:id' do
+    it 'returns http success' do
+      get '/real_time_geo_system/querier_panel/1234'
       expect(response).to have_http_status(:success)
     end
   end
