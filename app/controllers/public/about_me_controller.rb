fd970f0c8e0e6e6faf556a8790af9f7ecad22586
Hamdaan Khalid <khalid.hamdaan@gmail.com>
2023-01-13 10:20:12 -0800

    og birthday restored

--- app/controllers/public/about_me_controller.rb
+++ app/controllers/public/about_me_controller.rb
@@ -3,7 +3,7 @@
 module Public
   class AboutMeController < Public::BaseController
     def index
-      my_birthday = Time.new(1999, 1, 13)
+      my_birthday = Time.new(1999, 8, 10)
       today = Time.now
       @age = today.year - my_birthday.year
       if (my_birthday.month >  today.month or (my_birthday.month >= today.month and my_birthday.day > today.day))
