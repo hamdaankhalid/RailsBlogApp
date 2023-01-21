f90e82582276ba7ad4edcb39b89fae2be1578e52
Hamdaan Khalid <khalid.hamdaan@gmail.com>
2023-01-20 21:25:03 -0800

    cron job

--- app/jobs/daily_report_job.rb
+++ app/jobs/daily_report_job.rb
@@ -2,25 +2,11 @@ class DailyReportJob < ApplicationJob
   queue_as :default
 
   def perform(*args)
-    generate_report
-    send_email
-    # Clear the table :)
+    report =  RouteHit.group(:route).count
+    today = Date.today
+    AdminUser.all.each do |admin_user|
+      DailyReportMailer.with(routes_hit: report, email: admin_user.email, report_date: today).report.deliver_now
+    end
     RouteHit.destroy_all
-  end
-
-  private
-
-  def generate_report
-    @route_to_hit = []
-    # for all distinct routes in RouteHit table
-    # filter and gather those that are index based
-    # keep the rest as is
-    # put together a list of route and hit counts
-    
-    @hits_by_hour = []
-    # group the hits by the hours they were made
-  end
-
-  def send_email
   end
 end
