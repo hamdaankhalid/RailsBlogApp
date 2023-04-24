691e82912b89e3136a6f9b4f8ee73c514d5d63bc
Hamdaan Khalid <khalid.hamdaan@gmail.com>
2023-04-23 21:35:12 -0700

    register code reminder for admin

---
+++ app/admin/code_reminders.rb
@@ -0,0 +1,18 @@
+ActiveAdmin.register CodeReminder do
+
+  # See permitted parameters documentation:
+  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+  #
+  # Uncomment all parameters which should be permitted for assignment
+  #
+  # permit_params :email, :cadence, :cadence_unit
+  #
+  # or
+  #
+  # permit_params do
+  #   permitted = [:email, :cadence, :cadence_unit]
+  #   permitted << :other if params[:action] == 'create' && current_user.admin?
+  #   permitted
+  # end
+  
+end
